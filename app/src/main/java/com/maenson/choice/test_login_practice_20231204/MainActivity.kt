4033f65a294b25461a7968c3900ead58171bb3a8
maenson <barehands.go@gmail.com>
2023-12-03 18:28:13 +0900

    Initial commit

---
+++ app/src/main/java/com/maenson/choice/test_login_practice_20231204/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.maenson.choice.test_login_practice_20231204
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
